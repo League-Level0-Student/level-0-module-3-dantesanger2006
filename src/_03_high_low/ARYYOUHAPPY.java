499f3dac8dfb5a33a307988b44f1bf10f8496276
ilabstaff <ilabstaff@172.16.116.128>
2019-09-09 17:49:09 -0700

    AREYOUHAPPY

---
+++ src/_03_high_low/ARYYOUHAPPY.java
@@ -0,0 +1,71 @@
+package _03_high_low;
+
+import javax.swing.JOptionPane;
+
+public class ARYYOUHAPPY {
+public static void main(String[] args) {
+	String mun=JOptionPane.showInputDialog("ARE YOU HAPPY!!!!");   
+	if (mun.equals("yes")) {
+		JOptionPane.showMessageDialog(null,"keap doing what you are doing ");
+	}
+	else {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
--- src/_03_high_low/HighLowGame.java
+++ src/_03_high_low/HighLowGame.java
@@ -1,6 +1,5 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
 package _03_high_low;
 
 import java.util.Random;
@@ -10,35 +9,44 @@ import javax.swing.JOptionPane;
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(100)+1;
-		
+		// 3. Change this line to give you a random number between 1 - 100.
+		int random = new Random().nextInt(100) + 1;
+
 		// 2. Print out the random variable above
 		System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
-		
-			// 1. Ask the user for a guess using a pop-up window, and save their response 
-		String guse=JOptionPane.showInputDialog("gasa number");
-			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-		int number=Integer.parseInt(guse);
-			// 5. if the guess is correct
-		if (number==random) {
-			
-		
-		// 6. Win
-		JOptionPane.showMessageDialog(null, "you win");}
-		// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
-	
-		// 7. if the guess is high
-		
-				// 8. Tell them it's too high
-			// 9. if the guess is low
-				// 10. Tell them it's too low
+		for (int i = 0; i < 11; i++) {
 
-		// 13. Tell them they lose
+			// 1. Ask the user for a guess using a pop-up window, and save their response
+			String guse = JOptionPane.showInputDialog("guess number");
+			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
+			int number = Integer.parseInt(guse);
+			// 5. if the guess is correct
+			if (number == random) {
+
+				// 6. Win
+				JOptionPane.showMessageDialog(null, "you win");
+				i=11;
+			}
+			
+			// 7. if the guess is high
+			else if (random < number) {
+				JOptionPane.showMessageDialog(null, "your guess it's too high");
+			}
+			// 8. Tell them it's too high
+			else if (random>number ) {
+				JOptionPane.showMessageDialog(null, "guess is too low");
+			}
+			// 9. if the guess is low
+			// 10. Tell them it's too low
+
+			// 12. Tell them they lose
+			if (i==10) {
+				JOptionPane.showMessageDialog(null, "you lose");
+						
+			}
 		
+		}
 	}
 
 }
-
-
